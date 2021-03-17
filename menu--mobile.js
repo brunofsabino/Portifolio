e9bdba8866488c890aaa732b8b64cc669164e6a3
brunofsabino <brunoferrazsabino@gmail.com>
2021-03-17 13:25:10 -0300

    Update menu--mobile.js
    Atualização Portifolio 17/03/21

--- menu--mobile.js
+++ menu--mobile.js
@@ -11,156 +11,30 @@ const portfolioContent2 = document.querySelector('.portfolio--content')
 const contatoFormForm = document.querySelector('.contato-form form')
 const formFieldset = document.querySelector('.contato-form form fieldset')
 
-const tamanhoTela = document.querySelector('aside').clientWidth
+// const tamanhoTela = document.querySelector('aside').clientWidth
 
 // const linguagensConteudo3 = document.querySelector('.linguagensConteudo2')
 
 
 function responsivo2() {
    
-    
-    contatoForm2.style.display = 'none'
-    portfolioContent2.style.display = 'none'
-    banner2.style.display = 'flex'
-
-    if(tamanhoTela <= '425'){
-        
-
-        header2.style.top = '30px'
-        header2.style.height = '90px'
-    } else {
-        header2.style.top = '60px'
-        header2.style.height = '110px'
-    }
-    
-
+    window.scroll(0, 121)
     fecharMenuMobile()
 }
 
-
-function orcamento2() {
-    
-    
-
-    banner2.style.display = 'none'
-    portfolioContent2.style.display = 'none'
-    contatoForm2.style.display = 'flex'
-
-    contatoForm2.style.height = '400px'
-    contatoFormForm.style.height = '370px'
-    contatoFormForm.style.padding = '0px'
-    contatoFormForm.style.marginTop = '40px'
-    if(tamanhoTela <= '425'){
-        formFieldset.style.height = '360px'
-        header2.style.top = '30px'
-        header2.style.height = '90px'
-    } else {
-        formFieldset.style.height = '300px'
-        header2.style.top = '60px'
-        header2.style.height = '110px'
-        
-    }
-    
-    
-
-    fecharMenuMobile()
-}
 
 function contato2() {
     
-   
-
-    banner2.style.display = 'none'
-    portfolioContent2.style.display = 'none'
-    contatoForm2.style.display = 'flex'
-
-    contatoForm2.style.height = '400px'
-    contatoFormForm.style.height = '370px'
-    contatoFormForm.style.padding = '0px'
-    contatoFormForm.style.marginTop = '40px'
+    window.scroll(0, 1498)
     
-
-    if(tamanhoTela <= '425'){
-        
-
-        header2.style.top = '30px'
-        header2.style.height = '90px'
-        formFieldset.style.height = '360px'
-    } else {
-        header2.style.top = '60px'
-        header2.style.height = '110px'
-        formFieldset.style.height = '300px'
-    }
-    
-
+  
     fecharMenuMobile()
 }
 
 function portfolio2() {
     
 
-    
-
-    banner2.style.display = 'none'
-    contatoForm2.style.display = 'none'
-
-    portfolioContent2.style.display = 'flex'
-    
-    
-
-    if(tamanhoTela <= '666'){
-        console.log('oi oi')
-        portfolioContent2.style.top = '-45px'
-    } else {
-        portfolioContent2.style.top = '-115px'
-    }
-
-    linguagensConteudo2.style.overflow = 'auto'
-    linguagensConteudo.style.overflow = 'auto'
-
-    conteudoHtml2.style.height = '650px'
-    conteudoHtml.style.height = '650px'
-    conteudoHtml.style.marginTop = '325px'
-    conteudoHtml2.style.marginTop = '325px'
-    conteudoHtml.style.flexDirection = 'column'
-    conteudoHtml2.style.flexDirection = 'column'
-
-
-    conteudoCss2.style.height = '650px'
-    conteudoCss.style.height = '650px'
-    conteudoCss.style.marginTop = '325px'
-    conteudoCss2.style.marginTop = '325px'
-    conteudoCss.style.flexDirection = 'column'
-    conteudoCss2.style.flexDirection = 'column'
-
-    conteudoJavascript2.style.height = '650px'
-    conteudoJavascript.style.height = '650px'
-    conteudoJavascript.style.marginTop = '325px'
-    conteudoJavascript2.style.marginTop = '325px'
-    conteudoJavascript2.style.flexDirection = 'column'
-    conteudoJavascript.style.flexDirection = 'column'
-
-
-    conteudoPhp2.style.height = '650px'
-    conteudoPhp.style.height = '650px'
-    conteudoPhp2.style.marginTop = '325px'
-    conteudoPhp.style.marginTop = '325px'
-    conteudoPhp2.style.flexDirection = 'column'
-    conteudoPhp.style.flexDirection = 'column'
-
-
-    if(tamanhoTela <= '425'){
-        
-
-        header2.style.top = '30px'
-        header2.style.height = '90px'
-    } else {
-        header2.style.top = '60px'
-        header2.style.height = '110px'
-    }
-
-    portfolioContent2.style.height = '465px'
-
+    window.scroll(0, 726)
     fecharMenuMobile()
 }
 
