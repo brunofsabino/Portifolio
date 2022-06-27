82caeb708e0babb2cc5ee0aa99af52ffeaee8ed8
brunofsabino <brunoferrazsabino@gmail.com>
2022-06-27 18:57:54 -0300

    continuando portfolio

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,21 @@ const menuHamburger = document.querySelector('.menu-hamburguer')
 const menuL = document.querySelector('.menu-l')
 const menuLateral = document.querySelector('.menu-lateral')
 
+const area5Portifolio1 = document.querySelector('.area5-desc-content1')
+const area5Portifolio2 = document.querySelector('.area5-desc-content2')
+const area5FundoModal = document.querySelector('.area5-desc-content-modal')
+const areaModal = document.querySelector('.area5-modal1')
+const botaoFechar = document.querySelector('.close')
+const titleModal = document.querySelector('.area5-modal1 h2')
+const imgModal = document.querySelector('.area5-modal1-content-img')
+
+const session1 = document.querySelector('.area1 ')
+const alturaImg = document.querySelector('.area1 img').clientHeight
+const alturaTitle = document.querySelector('.area1-desc').clientHeight
+let paddingArea1 =  (alturaImg / 2) - alturaTitle
+console.log(paddingArea1)
+session1.style.padding = `${paddingArea1}px 0px`
+
 menuHamburger.addEventListener('click', click => {
     // menuLateral.classList.toggle('menu-lateral-display')
     menu.classList.toggle('menu-l')
@@ -11,3 +26,40 @@ menuHamburger.addEventListener('click', click => {
     body.classList.toggle('body-menu-lateral')
 })
 
+
+area5Portifolio1.addEventListener('click', abrirModal1)
+area5Portifolio2.addEventListener('click', abrirModal2)
+
+function abrirModal(){
+    body.style.overflow = 'hidden'
+    area5FundoModal.style.opacity = 0
+    area5FundoModal.style.display = 'flex'
+    areaModal.style.display = 'flex'
+    setTimeout(()=>{
+        area5FundoModal.style.opacity = 1
+    }, 300)
+}
+
+function abrirModal1() {
+    abrirModal()
+    titleModal.innerText = 'Quantos de você existe no Brasil?'
+    imgModal.style.backgroundImage = "url('../images/QuantosExisteBrasil.png')"
+    imgModal.style.backgroundColor = '#0E4959'
+    imgModal.style.backgroundPosition = 'center'
+}
+function abrirModal2() {
+    abrirModal()
+    titleModal.innerText = 'Qual é o valor do seu veículo?'
+    imgModal.style.backgroundImage = "url('../images/QualValorVeiculo.PNG')"
+    imgModal.style.backgroundColor = '#E0D8DE'
+    imgModal.style.backgroundPosition = 'bottom'
+}
+
+
+botaoFechar.addEventListener('click', fecharModal)
+
+function fecharModal(){
+    body.style.overflow = 'auto'
+    area5FundoModal.style.display = 'none'
+    areaModal.style.display = 'none'
+}
